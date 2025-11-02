dcff1042512ccd655c608f0ff20b09d92287237a
Shahid N. Shah <shahid@shah.org>
2025-11-02 18:47:49 -0500

    feat: introduce ${...} interpolation with --interpolate or -I PI #23

--- lib/task/execute.ts
+++ lib/task/execute.ts
@@ -1,3 +1,4 @@
+import { unsafeInterpolator } from "../universal/interpolate.ts";
 import {
   errorOnlyShellEventBus,
   shell,
@@ -6,13 +7,15 @@ import {
 import {
   errorOnlyTaskEventBus,
   executeDAG,
+  fail,
   ok,
   Task,
   TaskExecutionPlan,
   TaskExecutorBuilder,
   verboseInfoTaskEventBus,
 } from "../universal/task.ts";
-import { matchTaskNature } from "./cell.ts";
+import { safeJsonStringify } from "../universal/tmpl-literal-aide.ts";
+import { matchTaskNature, TaskCell } from "./cell.ts";
 
 export async function executeTasks<T extends Task>(
   plan: TaskExecutionPlan<T>,
@@ -20,6 +23,38 @@ export async function executeTasks<T extends Task>(
   summarize?: boolean,
 ) {
   type Context = { runId: string };
+  const unsafeInterp = unsafeInterpolator({ safeJsonStringify });
+
+  // "unsafely" means we're using JavaScript "eval"
+  async function interpolateUnsafely(
+    cell: TaskCell<string>,
+    ctx: Context,
+  ): Promise<
+    & { status: false | "unmodified" | "mutated" }
+    & ({ status: "mutated"; source: string } | {
+      status: "unmodified";
+      source: string;
+    } | {
+      status: false;
+      source: string;
+      error: unknown;
+    })
+  > {
+    const source = cell.source;
+    if (!cell.parsedPI?.hasEitherFlagOfType?.("interpolate", "I", "boolean")) {
+      return { status: "unmodified", source };
+    }
+
+    try {
+      // NOTE: This is intentionally unsafe. Do not feed untrusted content.
+      // Assume you're treating code cell blocks as fully trusted source code.
+      const mutated = await unsafeInterp.interpolate(source, { ...ctx, cell });
+      if (mutated !== source) return { status: "mutated", source: mutated };
+      return { status: "unmodified", source };
+    } catch (error) {
+      return { status: false, error, source };
+    }
+  }
 
   const sh = shell({
     bus: verbose
@@ -33,8 +68,13 @@ export async function executeTasks<T extends Task>(
       // then check if it's spawnable and handle spawning via shebang or Deno
       matchTaskNature("TASK"),
       async (cell, ctx) => {
-        await sh.auto(cell.source);
-        return ok(ctx);
+        const ir = await interpolateUnsafely(cell, ctx);
+        if (ir.status) {
+          await sh.auto(ir.source);
+          return ok(ctx);
+        } else {
+          return fail(ctx, ir.error);
+        }
       },
     )
     .build();
